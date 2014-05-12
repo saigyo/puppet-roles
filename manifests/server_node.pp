666aa467de6c78610fd3ef36304d8b92c30bf3ed
Markus Ackermann <markus.ackermann@acrolinx.com>
2014-05-12 12:46:45 +0200

    add everest and ubuntu user

--- manifests/server_node.pp
+++ manifests/server_node.pp
@@ -3,4 +3,60 @@ class roles::server_node {
   include profiles::mysql
   include profiles::mongodb
   include profiles::nodejs
+  
+  group { 'everest':
+    name   => 'everest',
+    ensure => present,
+    gid    => 222
+  }
+
+  user { 'everest':
+    require => Group['everest'],
+    name    => 'everest',
+    ensure  => present,
+    gid     => 'everest',
+    groups  => ['users','syslog'],
+    home    => '/mnt/everest',
+    system  => true
+  }
+
+  file { '/mnt/everest':
+    require => User['everest'],
+    path    => '/mnt/everest',
+    ensure  => directory,
+    owner   => 'everest',
+    group   => 'everest'
+  }
+
+  file { '/home/everest':
+    require => File['/mnt/everest'],
+    ensure  => link,
+    target  => '/mnt/everest',
+    force   => true
+  }
+  
+  group { 'ubuntu':
+    name   => 'everest',
+    ensure => present,
+    gid    => 1000
+  }
+
+  user { 'ubuntu':
+    require => Group['ubuntu'],
+    name    => 'ubuntu',
+    uid     => 1000,
+    gid     => 'ubuntu',
+    groups  => ['ubuntu','adm'],
+    home    => '/home/ubuntu',
+    system  => true,
+    ensure  => present
+  }
+
+  file { '/home/ubuntu':
+    require => User['ubuntu'],
+    path    => '/home/ubuntu',
+    ensure  => directory,
+    owner   => 'ubuntu',
+    group   => 'ubuntu'
+  }
 }
